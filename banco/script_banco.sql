89bd973374f9f4de9033855a7e2a812eddb652ea
Willian Gomes <williang181@gmail.com>
2018-09-03 11:43:17 -0300

    Script do banco atualizado

--- banco/script_banco.sql
+++ banco/script_banco.sql
@@ -1,5 +1,59 @@
 CREATE DATABASE Farmacia;
 
+CREATE TABLE `dispensas` (
+  `idDispensa` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `dataDispensa` date NOT NULL,
+  `idRetirante` int(11) NOT NULL,
+  `idPaciente` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  PRIMARY KEY (`idDispensa`),
+  KEY `FK_Retirante` (`idRetirante`),
+  KEY `FK_Paciente` (`idPaciente`),
+  KEY `FK_Usuario` (`idUsuario`),
+  CONSTRAINT `dispensas_ibfk_1` FOREIGN KEY (`idRetirante`) REFERENCES `retirantes` (`idRetirante`),
+  CONSTRAINT `dispensas_ibfk_2` FOREIGN KEY (`idPaciente`) REFERENCES `pacientes` (`idPaciente`),
+  CONSTRAINT `dispensas_ibfk_3` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+CREATE TABLE `dispensas_medicamentos` (
+  `idDispensa` int(11) NOT NULL,
+  `idMedicamento` int(11) NOT NULL,
+  KEY `FK_Dispensa` (`idDispensa`),
+  KEY `FK_Medicamento_dis` (`idMedicamento`),
+  CONSTRAINT `FK_Dispensa` FOREIGN KEY (`idDispensa`) REFERENCES `dispensas` (`idDispensa`),
+  CONSTRAINT `FK_Medicamento_dis` FOREIGN KEY (`idMedicamento`) REFERENCES `medicamentos` (`idMedicamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+CREATE TABLE `estoque` (
+  `lote` varchar(45) NOT NULL,
+  `idMedicamento` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `dataVencimento` date NOT NULL,
+  PRIMARY KEY (`lote`),
+  KEY `idMedicamento` (`idMedicamento`),
+  CONSTRAINT `estoque_ibfk_1` FOREIGN KEY (`idMedicamento`) REFERENCES `medicamentos` (`idMedicamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+CREATE TABLE `medicamentos` (
+  `idMedicamento` int(11) NOT NULL AUTO_INCREMENT,
+  `nomeMedicamento` varchar(255) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `nomeFabricante` varchar(255) NOT NULL,
+  `composicao` varchar(45) NOT NULL,
+  `dosagem` double NOT NULL,
+  PRIMARY KEY (`idMedicamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+CREATE TABLE `medicamentos_receitas` (
+  `idMedicamento` int(11) NOT NULL,
+  `idReceita` int(11) NOT NULL,
+  KEY `FK_Medicamento` (`idMedicamento`),
+  KEY `FK_Receita` (`idReceita`),
+  CONSTRAINT `FK_Medicamento` FOREIGN KEY (`idMedicamento`) REFERENCES `medicamentos` (`idMedicamento`),
+  CONSTRAINT `FK_Receita` FOREIGN KEY (`idReceita`) REFERENCES `receitas` (`idReceita`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
 CREATE TABLE `pacientes` (
   `idPaciente` int(11) NOT NULL AUTO_INCREMENT,
   `cpfPaciente` varchar(11) NOT NULL,
@@ -15,8 +69,15 @@ CREATE TABLE `pacientes` (
   `complemento` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`idPaciente`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
+CREATE TABLE `receitas` (
+  `idReceita` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(255) NOT NULL,
+  `CRM` varchar(45) NOT NULL,
+  `validadeReceita` date NOT NULL,
+  PRIMARY KEY (`idReceita`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
 CREATE TABLE `retirantes` (
   `idRetirante` int(11) NOT NULL AUTO_INCREMENT,
@@ -32,34 +93,24 @@ CREATE TABLE `retirantes` (
   `numEndereco` int(11) NOT NULL,
   `complemento` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `parentesco` varchar(55) NOT NULL,
+  `vincolo` varchar(45) NOT NULL,
   PRIMARY KEY (`idRetirante`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
-CREATE TABLE `retirantesPacientes` (
-  `idPacienteFk` int(11) NOT NULL,
-  `idRetiranteFk` int(11) NOT NULL,
-  KEY `idPacienteFk` (`idPacienteFk`),
-  KEY `idRetiranteFk` (`idRetiranteFk`),
-  CONSTRAINT `retirantesPacientes_ibfk_1` FOREIGN KEY (`idPacienteFk`) REFERENCES `pacientes` (`idPaciente`),
-  CONSTRAINT `retirantesPacientes_ibfk_2` FOREIGN KEY (`idRetiranteFk`) REFERENCES `retirantes` (`idRetirante`)
+CREATE TABLE `retirantes_pacientes` (
+  `idPaciente` int(11) NOT NULL,
+  `idRetirante` int(11) NOT NULL,
+  KEY `FK_Paciente` (`idPaciente`),
+  KEY `FK_Retirante` (`idRetirante`),
+  CONSTRAINT `FK_Paciente` FOREIGN KEY (`idPaciente`) REFERENCES `pacientes` (`idPaciente`),
+  CONSTRAINT `FK_Retirante` FOREIGN KEY (`idRetirante`) REFERENCES `retirantes` (`idRetirante`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
 CREATE TABLE `usuarios` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nomeUsuario` varchar(255) NOT NULL,
+  `emailUsuario` varchar(255) NOT NULL,
   `login` varchar(50) NOT NULL,
   `senha` varchar(255) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1
-
-CREATE TABLE `medicamentos` (
-  `idMedicamento` int(11) NOT NULL AUTO_INCREMENT,
-  `nomeMedicamento` varchar(255) NOT NULL,
-  `descricao` varchar(255) NOT NULL,
-  `nomeFabricante` varchar(255) NOT NULL,
-  `composicao` varchar(45) NOT NULL,
-  `dosagem` double NOT NULL,
-  PRIMARY KEY (`idMedicamento`)
+  PRIMARY KEY (`idUsuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
