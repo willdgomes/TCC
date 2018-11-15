004adf093841c1de24c45f19c217df4dba977aca
Willian Gomes <williang181@gmail.com>
2018-11-15 16:08:13 -0200

    Impl Receita

--- src/java/Beans/Receita.java
+++ src/java/Beans/Receita.java
@@ -5,10 +5,49 @@
  */
 package Beans;
 
+import java.io.Serializable;
+import java.sql.Date;
+
 /**
  *
  * @author gomes
  */
-public class Receita {
+public class Receita implements Serializable {
     
+    private Integer id;
+    private String nomeMedico;
+    private String crmMedico;
+    private Date dataVencimentoReceita;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeMedico() {
+        return nomeMedico;
+    }
+
+    public void setNomeMedico(String nomeMedico) {
+        this.nomeMedico = nomeMedico;
+    }
+
+    public String getCrmMedico() {
+        return crmMedico;
+    }
+
+    public void setCrmMedico(String crmMedico) {
+        this.crmMedico = crmMedico;
+    }
+
+    public Date getDataVencimentoReceita() {
+        return dataVencimentoReceita;
+    }
+
+    public void setDataVencimentoReceita(Date dataVencimentoReceita) {
+        this.dataVencimentoReceita = dataVencimentoReceita;
+    }
 }
